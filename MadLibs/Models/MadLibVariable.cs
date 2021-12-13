a1ec2c64d0d42f1f3025a20cd106baf524ae45c8
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-13 13:19:26 -0800

    Add auto-implemented properties

---
+++ MadLibs/Models/MadLibVariable.cs
@@ -0,0 +1,16 @@
+namespace MadLibs.Models
+{
+  public class MadLibVariable
+  {
+    public string Color { get; set; }
+    public string Superlative { get; set; }
+    public string Adjective1 { get; set; }
+    public string BodyPart1 { get; set; }
+    public string BodyPart2 {get; set; }
+    public string Noun { get; set; }
+    public string Animal { get; set; }
+    public string Adjective2 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Adjective4 { get; set; }
+  }
+}
